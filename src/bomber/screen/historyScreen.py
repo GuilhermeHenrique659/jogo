ee843f9b71b2090609be0e6074a1d3139cfb9a45
GuilhermeHenrique659 <guilhermehenrique.o@hotmal.com>
2023-10-03 20:04:17 -0300

    add history

--- src/bomber/screen/historyScreen.py
+++ src/bomber/screen/historyScreen.py
@@ -6,19 +6,22 @@ from common.text import Text
 class HistoryScreen(Screen):
     def __init__(self, action) -> None:
         super().__init__('assets/historyBg.png')
-        self.history_text_1 = Text('Durante a união sovietica, familares acusada de atos anti sovientico', 'black', 32, Point((2, 5)).convert_to_point())    
-        self.history_text_2 = Text('eram jogadas no gulag, onde seus primogenito lutam com bombas,', 'black', 32, Point((2, 6)).convert_to_point())
-        self.history_text_3 = Text('para que decida quem irá ter acesso a comida.', 'black', 32, Point((2, 7)).convert_to_point())
-        self.init_text = Text('Proximo', 'black', 32, Point((3, 12)).convert_to_point())
+        self.history_text = [
+            'Em um futuro distópico, o mundo está mergulhado em caos e anarquia.',
+            'As nações caíram, e o que restou da ',
+            'União Soviética é um território devastado pela guerra e pela radiação.',
+            'Duas famílias, os Volkov e os Ivanov, emergem',
+            'como as últimas esperanças de sobrevivência  em meio ao deserto destruído.'
+        ]
+        self.init_text = Text('Proximo', 'white', 32, Point((3, 12)).convert_to_point())
         self.cursor = Cursor(Point((2, 12)).convert_to_point(), [12])
         self.action = action
 
 
     def render(self):
         self.cover_screen_with_image()
-        self.history_text_1.render(self.display)
-        self.history_text_2.render(self.display)
-        self.history_text_3.render(self.display)
+        for index, text in enumerate(self.history_text):
+            Text(text, 'white', 24, Point((3, 4 + index)).convert_to_point()).render(self.display)
         self.init_text.render(self.display)
         self.cursor.render()
         if self.cursor.point.compare_tile_y(12) and self.cursor.confirm:
